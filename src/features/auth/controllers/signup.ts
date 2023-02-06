0ea64f573bc211c655c8b8b80383613a78b23434
Vikash <kvvik2020@gmail.com>
2023-02-06 23:07:42 +0530

    singup auth added

---
+++ src/features/auth/controllers/signup.ts
@@ -0,0 +1,58 @@
+import { ObjectId } from 'mongodb';
+import {Request,Response} from 'express';
+import { joiValidation } from '@global/decorators/joi-validation.decorator';
+import { signupSchema } from '@auth/schemes/signup';
+import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface';
+import { authService } from '@service/db/auth.service';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { Helpers } from '@global/helpers/helpers';
+import { UploadApiResponse } from 'cloudinary';
+import { uploads } from '@global/helpers/cloudinary-upload';
+import  HTTP_STATUS  from 'http-status-codes';
+
+export class SignUp{
+    @joiValidation(signupSchema)
+    public async create(req:Request, res:Response): Promise<void>{
+      const {username, email, password, avatarColor, avatarImage} = req.body;
+      const checkIfUserExist: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
+
+      if(checkIfUserExist){
+        throw new BadRequestError('Invalid credentials');
+      }
+
+      const authObjectId: ObjectId = new ObjectId();
+      const userObjectId: ObjectId = new ObjectId();
+      const uId = `${Helpers.generateRandomIntegers(12)}`;
+      const authdata : IAuthDocument = SignUp.prototype.signupData({
+        _id:authObjectId,
+        uId,
+        username,
+        email,
+        password,
+        avatarColor
+      });
+      const result : UploadApiResponse = await uploads(avatarImage, `${userObjectId}`,true,true) as UploadApiResponse;
+
+      if(!result?.public_id){
+        throw new BadRequestError('File upload: Error occured. Try again.');
+      }
+
+      res.status(HTTP_STATUS.CREATED).json({message: 'User created successfully',authdata});
+
+    }
+
+    private signupData(data: ISignUpData): IAuthDocument{
+      const {_id, username,email, uId, password, avatarColor} = data;
+
+      return {
+        _id,
+        uId,
+        username: Helpers.firstLetterUppercase(username),
+        email: Helpers.lowerCase(email),
+        password,
+        avatarColor,
+        createdAt: new Date(),
+
+      } as  IAuthDocument;
+    }
+  }
