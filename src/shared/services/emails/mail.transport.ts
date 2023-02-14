e071e065a0076d51761c36d884a4c8ab2ccf640a
Vikash <kvvik2020@gmail.com>
2023-02-14 18:32:01 +0530

    added imports

---
+++ src/shared/services/emails/mail.transport.ts
@@ -0,0 +1,6 @@
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import Logger from 'bunyan';
+import sendGridMail from '@sendgrid/mail';
+import {config} from '@root/config';
+
